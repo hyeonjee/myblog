26ed4bb00311c4a5c1e1bce20bb8b1f12d5cbc63
hyeonjee <61783447+hyeonjee@users.noreply.github.com>
2020-07-01 19:07:30 +0900

    first commit

---
+++ lovely/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import first, second, third
+
+app_name="lovely"
+urlpatterns = [
+    path('first/', first, name="first"),
+    path('second/', second, name="second"),
+    path('third/', third, name="third"),
+]
+     
